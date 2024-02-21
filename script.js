4df7087a65d6876d958706d12f3dd2277c445f0b
dishag07 <dishag422@gmail.com>
2024-02-21 13:49:13 -0500

    name done but not styled

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function start() {
+    let name = document.getElementById('userName').value;
+        document.getElementById('response-text').innerText = 'Welcome ' + name;
+        document.getElementById('nameInput').classList.add('d-none');
+}
+
