5b3ff8814ec3c669e01c296f0552748b649206a1
dishag07 <dishag422@gmail.com>
2024-02-22 14:56:22 -0500

    meow

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function start() {
     let name = document.getElementById('userName').value;
-        document.getElementById('response-text').innerText = 'Welcome ' + name;
+        document.getElementById('response-text').innerText = 'Welcome ' + name + ' & Good Luck!';
         document.getElementById('nameInput').classList.add('d-none');
 }
 
