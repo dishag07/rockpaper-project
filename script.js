b363383bfaa7a97e2fe2ac42acae5f253e9c1aeb
dishag07 <dishag422@gmail.com>
2024-02-27 14:54:12 -0500

    god knows

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-
-
 function start() {
-    let name = document.getElementById('userName').value;
-        document.getElementById('response-text').innerText = 'Welcome ' + name + ' & Good Luck!';
-        document.getElementById('nameInput').classList.add('d-none');
+  let name = document.getElementById("userName").value;
+  document.getElementById("response-text").innerText =
+    "Welcome " + name + " & Good Luck!";
+  document.getElementById("nameInput").classList.add("d-none");
+
 }
+
+function playGame(userChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    // :(((((
+
+}
+
+
 
