6e8b1d8e220b9f2c9cdc96c97438b999044e3fcd
dishag07 <dishag422@gmail.com>
2024-03-01 13:36:43 -0500

    done :)

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
+let userScore = 0;
+let compScore = 0;
+let userName
+let userChoice
+let computerChoice
+let roundResult
+
 function start() {
-  let name = document.getElementById("userName").value;
+  userName = document.getElementById("userName").value;
   document.getElementById("response-text").innerText =
-    "Welcome " + name + " & Good Luck!";
+    "Welcome " + userName + " & Good Luck!";
   document.getElementById("nameInput").classList.add("d-none");
 
 }
 
-function playGame(userChoice) {
-    const choices = ['rock', 'paper', 'scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    // :(((((
+function playGame(userSelection) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerSelection = choices[Math.floor(Math.random() * 3)];
+  userChoice = userSelection
+  computerChoice = computerSelection
+
+  if (userSelection === computerSelection) {
+    roundResult = "It's a tie!"
+
+  } else if ((userSelection === 'rock' && computerSelection === 'scissors') ||
+    (userSelection === 'paper' && computerSelection === 'rock') ||
+    (userSelection === 'scissors' && computerSelection === 'paper')) {
+    userScore++;
+    roundResult = "You win!"
+   
+    updateDisplayInfo()
+    isGameOver()
+  } else {
+    compScore++;
+    roundResult = "You lose!"
+    
+    updateDisplayInfo()
+    isGameOver()
+
+
+    //update score for comp compScore++
+    //display computer score 
+  }
 
 }
 
 
+function isGameOver(){
 
+  if (userScore === 3) {
+    console.log("user wins")
+
+    document.getElementById('result').innerText = `Game Over. ${userName} has won with a score of ${userScore} to ${compScore}`;
+    location.reload();
+} else if (compScore === 3) {
+  console.log("comp wins")
+  document.getElementById('result').innerText = `Game Over. Comp has won with a score of ${compScore} to ${userScore}`;
+  location.reload();
+}
+  }
+
+  function updateDisplayInfo (){
+    document.getElementById('userScore').innerText = userScore;
+    document.getElementById('compScore').innerText = compScore;
+    document.getElementById('result').innerText = `You chose ${userChoice}, computer chose ${computerChoice}. ${roundResult}`;
+  }
